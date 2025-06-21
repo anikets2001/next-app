626dd2de89f800f0be27846a40cd5f6550c90967
Aniket Singh <aniketsingh90126@gmail.com>
2025-06-21 22:26:56 +0530

    feat: routing

---
+++ src/Notes/03_Routing.js
@@ -0,0 +1,10 @@
+/*---
+
+Basic Routing:
+
+- Next.js follow file based routing
+- To create a new route we have to create a folder in src/app with the exact name we want the route
+   and inside that folder create a page.js file
+- go to that created route in browser and you will see that component
+
+---*/
