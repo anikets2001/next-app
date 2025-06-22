20d06e0dc84fd7e9e60ba80a104e98f0ca6d5011
Aniket Singh <aniketsingh90126@gmail.com>
2025-06-22 15:58:58 +0530

    feat: data fetching in client and serer components

---
+++ src/app/products/page.js
@@ -0,0 +1,36 @@
+// this is a server component for fetching products api
+import './products.css'
+
+const apiUrl = "https://dummyjson.com/products?limit=50";
+
+async function Products() {
+  const response = await fetch(apiUrl);
+  const json = await response.json();
+  return json.products;
+}
+
+export default async function page() {
+  const products = await Products();
+
+  return (
+    <div className="products-wrapper">
+      <h1>Products List ({products.length})</h1>
+      <div className="card-wrapper">
+        {products.map((product) => (
+          <div key={product?.id} className="product-card">
+            <div className="card-heading-wrapper">
+              <p>Title: {product?.title}</p>
+              <p>Price: {product?.price}</p>
+            </div>
+            <div className="image-wrapper">
+              <img src={product?.thumbnail} alt={product?.title} />
+            </div>
+            <div>
+              <p>Description: {product?.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
