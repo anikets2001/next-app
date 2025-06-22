bbffe8db05ee8d223a81b00bbc0ca29d7f4ef407
Aniket Singh <aniketsingh90126@gmail.com>
2025-06-22 19:59:51 +0530

    feat: css modules

---
+++ src/Notes/10_styling.js
@@ -0,0 +1,19 @@
+/*---
+
+To style a component in next js we have different approaches
+1. Global CSS 
+2. CSS Modules (product.module.css)
+3. Tailwind CSS 
+4. Styled Components
+5. Sass / SCSS
+---*/
+
+/*---
+
+using css modules is recommended when working with next js
+step1: create a file example.module.css
+step2: import this file in the component you want to you it
+step3: import style from './product.module.css';
+step4: use as className={style.header}
+
+---*/
